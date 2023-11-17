3d349160d720dc8369880bea4bd644d3d75c537c
Md Rushd Al Amin <72799488+vitamin2025@users.noreply.github.com>
2023-11-17 07:06:59 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,79 +1,73 @@
 // create web server
-// 1. load express
-var express = require("express");
-// 2. create an instance of express
-var app = express();
-// 3. load body-parser
-var bodyParser = require("body-parser");
-// 4. load mongoose
-var mongoose = require("mongoose");
-// 5. load the model
-var Comment = require("./models/comment");
-// 6. load the model
-var Campground = require("./models/campground");
-// 7. load the seed file
-var seedDB = require("./seeds");
+// create router object
+const express = require('express');
+const comments = require('../models/comments');
+const router = express.Router();
 
-// 8. connect to the database
-mongoose.connect("mongodb://localhost/yelp_camp_v5");
-// 9. tell express to use body-parser
-app.use(bodyParser.urlencoded({extended: true}));
-// 10. set the view engine
-app.set("view engine", "ejs");
-// 11. serve the public directory
-app.use(express.static(__dirname + "/public"));
-// 12. seed the database
-seedDB();
-
-// ROUTES
-// 1. landing page
-app.get("/", function(req, res){
-    res.render("landing");
+// get all comments
+router.get('/', (req, res) => {
+    comments.getAll()
+    .then((data) => {
+        res.json(data);
+    })
+    .catch((err) => {
+        res.status(400).json(err);
+    })
 });
 
-// 2. INDEX - show all campgrounds
-app.get("/campgrounds", function(req, res){
-    // get all campgrounds from the database
-    Campground.find({}, function(err, allCampgrounds){
-        if(err){
-            console.log("Error: " + err);
-        } else {
-            // render the campgrounds page
-            res.render("campgrounds/index", {campgrounds: allCampgrounds});
-        }
-    });
+// get comment by id
+router.get('/:id', (req, res) => {
+    comments.getById(req.params.id)
+    .then((data) => {
+        res.json(data);
+    })
+    .catch((err) => {
+        res.status(400).json(err);
+    })
 });
 
-// 3. CREATE - add new campground to DB
-app.post("/campgrounds", function(req, res){
-    // get data from form and add to campgrounds array
-    var newCampground = {name: req.body.name, image: req.body.image, description: req.body.description};
-    // create a new campground and save to the database
-    Campground.create(newCampground, function(err, newlyCreated){
-        if(err){
-            console.log("Error: " + err);
-        } else {
-            // redirect back to campgrounds page
-            res.redirect("/campgrounds");
-        }
-    });
+// get comments by post id
+router.get('/post/:id', (req, res) => {
+    comments.getByPostId(req.params.id)
+    .then((data) => {
+        res.json(data);
+    })
+    .catch((err) => {
+        res.status(400).json(err);
+    })
 });
 
-// 4. NEW - show form to create new campground
-app.get("/campgrounds/new", function(req, res){
-    res.render("campgrounds/new");
+// create comment
+router.post('/', (req, res) => {
+    comments.create(req.body)
+    .then((data) => {
+        res.json(data);
+    })
+    .catch((err) => {
+        res.status(400).json(err);
+    })
 });
 
-// 5. SHOW - shows more info about one campground
-app.get("/campgrounds/:id", function(req, res){
-    // find the campground with the provided ID
-    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
-        if(err){
-            console.log("Error: " + err);
-        } else {
-            console.log(foundCampground);
-            // render the show template with that campground
-            res.render("campgrounds/show", {campground: foundCampground});
-        }
-    });
+// update comment by id
+router.put('/:id', (req, res) => {
+    comments.updateById(req.params.id, req.body)
+    .then((data) => {
+        res.json(data);
+    })
+    .catch((err) => {
+        res.status(400).json(err);
+    })
 });
+
+// delete comment by id
+router.delete('/:id', (req, res) => {
+    comments.deleteById(req.params.id)
+    .then((data) => {
+        res.json(data);
+    })
+    .catch((err) => {
+        res.status(400).json(err);
+    })
+});
+
+module.exports = router;
